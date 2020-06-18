a21fed8f63d0d62fe678adfc857734853f6ff541
zhoujianping <zhoujianping@jiuxiniot.com>
2020-06-18 16:56:07 +0800

    项目初始化

--- gen.bat
+++ gen.bat
@@ -1,2 +1,3 @@
 protoc --proto_path=. --go_out=../../../ --micro_out=../../../ ./role.proto
 protoc --proto_path=. --go_out=../../../ --micro_out=../../../ ./login.proto
+protoc --proto_path=. --go_out=../../../ --micro_out=../../../ ./unified.proto
