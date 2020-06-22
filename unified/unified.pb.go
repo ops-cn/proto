128f5c7e6563cab4fdfcd3315e7c4b25ed7cdc38
zhoujianping <zhoujianping@jiuxiniot.com>
2020-06-22 17:58:40 +0800

    目录结构调整

--- unified/unified.pb.go
+++ unified/unified.pb.go
@@ -135,6 +135,196 @@ func (x *Response) GetItems() *any.Any {
 	return nil
 }
 
+type ListResult struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Items      *any.Any          `protobuf:"bytes,1,opt,name=items,proto3" json:"items,omitempty"`
+	Pagination *PaginationResult `protobuf:"bytes,2,opt,name=Pagination,proto3" json:"Pagination,omitempty"`
+}
+
+func (x *ListResult) Reset() {
+	*x = ListResult{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_unified_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ListResult) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ListResult) ProtoMessage() {}
+
+func (x *ListResult) ProtoReflect() protoreflect.Message {
+	mi := &file_unified_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ListResult.ProtoReflect.Descriptor instead.
+func (*ListResult) Descriptor() ([]byte, []int) {
+	return file_unified_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *ListResult) GetItems() *any.Any {
+	if x != nil {
+		return x.Items
+	}
+	return nil
+}
+
+func (x *ListResult) GetPagination() *PaginationResult {
+	if x != nil {
+		return x.Pagination
+	}
+	return nil
+}
+
+type PaginationResult struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Total    int32  `protobuf:"varint,1,opt,name=Total,proto3" json:"Total,omitempty"`
+	Current  uint32 `protobuf:"varint,2,opt,name=Current,proto3" json:"Current,omitempty"`
+	PageSize uint32 `protobuf:"varint,3,opt,name=PageSize,proto3" json:"PageSize,omitempty"`
+}
+
+func (x *PaginationResult) Reset() {
+	*x = PaginationResult{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_unified_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PaginationResult) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PaginationResult) ProtoMessage() {}
+
+func (x *PaginationResult) ProtoReflect() protoreflect.Message {
+	mi := &file_unified_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PaginationResult.ProtoReflect.Descriptor instead.
+func (*PaginationResult) Descriptor() ([]byte, []int) {
+	return file_unified_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *PaginationResult) GetTotal() int32 {
+	if x != nil {
+		return x.Total
+	}
+	return 0
+}
+
+func (x *PaginationResult) GetCurrent() uint32 {
+	if x != nil {
+		return x.Current
+	}
+	return 0
+}
+
+func (x *PaginationResult) GetPageSize() uint32 {
+	if x != nil {
+		return x.PageSize
+	}
+	return 0
+}
+
+// PaginationParam 分页查询条件
+type PaginationParam struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Pagination bool   `protobuf:"varint,1,opt,name=Pagination,proto3" json:"Pagination,omitempty"`
+	OnlyCount  bool   `protobuf:"varint,2,opt,name=OnlyCount,proto3" json:"OnlyCount,omitempty"`
+	Current    uint32 `protobuf:"varint,3,opt,name=Current,proto3" json:"Current,omitempty"`
+	PageSize   uint32 `protobuf:"varint,4,opt,name=PageSize,proto3" json:"PageSize,omitempty"`
+}
+
+func (x *PaginationParam) Reset() {
+	*x = PaginationParam{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_unified_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PaginationParam) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PaginationParam) ProtoMessage() {}
+
+func (x *PaginationParam) ProtoReflect() protoreflect.Message {
+	mi := &file_unified_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PaginationParam.ProtoReflect.Descriptor instead.
+func (*PaginationParam) Descriptor() ([]byte, []int) {
+	return file_unified_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *PaginationParam) GetPagination() bool {
+	if x != nil {
+		return x.Pagination
+	}
+	return false
+}
+
+func (x *PaginationParam) GetOnlyCount() bool {
+	if x != nil {
+		return x.OnlyCount
+	}
+	return false
+}
+
+func (x *PaginationParam) GetCurrent() uint32 {
+	if x != nil {
+		return x.Current
+	}
+	return 0
+}
+
+func (x *PaginationParam) GetPageSize() uint32 {
+	if x != nil {
+		return x.PageSize
+	}
+	return 0
+}
+
 var File_unified_proto protoreflect.FileDescriptor
 
 var file_unified_proto_rawDesc = []byte{
@@ -149,10 +339,32 @@ var file_unified_proto_rawDesc = []byte{
 	0x64, 0x65, 0x73, 0x63, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x64, 0x65, 0x73, 0x63,
 	0x12, 0x2a, 0x0a, 0x05, 0x69, 0x74, 0x65, 0x6d, 0x73, 0x18, 0x04, 0x20, 0x01, 0x28, 0x0b, 0x32,
 	0x14, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75,
-	0x66, 0x2e, 0x41, 0x6e, 0x79, 0x52, 0x05, 0x69, 0x74, 0x65, 0x6d, 0x73, 0x42, 0x21, 0x5a, 0x1f,
-	0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6f, 0x70, 0x73, 0x2d, 0x63,
-	0x6e, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x75, 0x6e, 0x69, 0x66, 0x69, 0x65, 0x64, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x66, 0x2e, 0x41, 0x6e, 0x79, 0x52, 0x05, 0x69, 0x74, 0x65, 0x6d, 0x73, 0x22, 0x73, 0x0a, 0x0a,
+	0x4c, 0x69, 0x73, 0x74, 0x52, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x12, 0x2a, 0x0a, 0x05, 0x69, 0x74,
+	0x65, 0x6d, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x14, 0x2e, 0x67, 0x6f, 0x6f, 0x67,
+	0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x41, 0x6e, 0x79, 0x52,
+	0x05, 0x69, 0x74, 0x65, 0x6d, 0x73, 0x12, 0x39, 0x0a, 0x0a, 0x50, 0x61, 0x67, 0x69, 0x6e, 0x61,
+	0x74, 0x69, 0x6f, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x19, 0x2e, 0x75, 0x6e, 0x69,
+	0x66, 0x69, 0x65, 0x64, 0x2e, 0x50, 0x61, 0x67, 0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x52,
+	0x65, 0x73, 0x75, 0x6c, 0x74, 0x52, 0x0a, 0x50, 0x61, 0x67, 0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f,
+	0x6e, 0x22, 0x5e, 0x0a, 0x10, 0x50, 0x61, 0x67, 0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x52,
+	0x65, 0x73, 0x75, 0x6c, 0x74, 0x12, 0x14, 0x0a, 0x05, 0x54, 0x6f, 0x74, 0x61, 0x6c, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x05, 0x52, 0x05, 0x54, 0x6f, 0x74, 0x61, 0x6c, 0x12, 0x18, 0x0a, 0x07, 0x43,
+	0x75, 0x72, 0x72, 0x65, 0x6e, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x07, 0x43, 0x75,
+	0x72, 0x72, 0x65, 0x6e, 0x74, 0x12, 0x1a, 0x0a, 0x08, 0x50, 0x61, 0x67, 0x65, 0x53, 0x69, 0x7a,
+	0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x08, 0x50, 0x61, 0x67, 0x65, 0x53, 0x69, 0x7a,
+	0x65, 0x22, 0x85, 0x01, 0x0a, 0x0f, 0x50, 0x61, 0x67, 0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f, 0x6e,
+	0x50, 0x61, 0x72, 0x61, 0x6d, 0x12, 0x1e, 0x0a, 0x0a, 0x50, 0x61, 0x67, 0x69, 0x6e, 0x61, 0x74,
+	0x69, 0x6f, 0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x08, 0x52, 0x0a, 0x50, 0x61, 0x67, 0x69, 0x6e,
+	0x61, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x1c, 0x0a, 0x09, 0x4f, 0x6e, 0x6c, 0x79, 0x43, 0x6f, 0x75,
+	0x6e, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x08, 0x52, 0x09, 0x4f, 0x6e, 0x6c, 0x79, 0x43, 0x6f,
+	0x75, 0x6e, 0x74, 0x12, 0x18, 0x0a, 0x07, 0x43, 0x75, 0x72, 0x72, 0x65, 0x6e, 0x74, 0x18, 0x03,
+	0x20, 0x01, 0x28, 0x0d, 0x52, 0x07, 0x43, 0x75, 0x72, 0x72, 0x65, 0x6e, 0x74, 0x12, 0x1a, 0x0a,
+	0x08, 0x50, 0x61, 0x67, 0x65, 0x53, 0x69, 0x7a, 0x65, 0x18, 0x04, 0x20, 0x01, 0x28, 0x0d, 0x52,
+	0x08, 0x50, 0x61, 0x67, 0x65, 0x53, 0x69, 0x7a, 0x65, 0x42, 0x21, 0x5a, 0x1f, 0x67, 0x69, 0x74,
+	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6f, 0x70, 0x73, 0x2d, 0x63, 0x6e, 0x2f, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x75, 0x6e, 0x69, 0x66, 0x69, 0x65, 0x64, 0x62, 0x06, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -167,19 +379,24 @@ func file_unified_proto_rawDescGZIP() []byte {
 	return file_unified_proto_rawDescData
 }
 
-var file_unified_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_unified_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
 var file_unified_proto_goTypes = []interface{}{
-	(*Request)(nil),  // 0: unified.Request
-	(*Response)(nil), // 1: unified.Response
-	(*any.Any)(nil),  // 2: google.protobuf.Any
+	(*Request)(nil),          // 0: unified.Request
+	(*Response)(nil),         // 1: unified.Response
+	(*ListResult)(nil),       // 2: unified.ListResult
+	(*PaginationResult)(nil), // 3: unified.PaginationResult
+	(*PaginationParam)(nil),  // 4: unified.PaginationParam
+	(*any.Any)(nil),          // 5: google.protobuf.Any
 }
 var file_unified_proto_depIdxs = []int32{
-	2, // 0: unified.Response.items:type_name -> google.protobuf.Any
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	5, // 0: unified.Response.items:type_name -> google.protobuf.Any
+	5, // 1: unified.ListResult.items:type_name -> google.protobuf.Any
+	3, // 2: unified.ListResult.Pagination:type_name -> unified.PaginationResult
+	3, // [3:3] is the sub-list for method output_type
+	3, // [3:3] is the sub-list for method input_type
+	3, // [3:3] is the sub-list for extension type_name
+	3, // [3:3] is the sub-list for extension extendee
+	0, // [0:3] is the sub-list for field type_name
 }
 
 func init() { file_unified_proto_init() }
@@ -212,6 +429,42 @@ func file_unified_proto_init() {
 				return nil
 			}
 		}
+		file_unified_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ListResult); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_unified_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PaginationResult); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_unified_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PaginationParam); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -219,7 +472,7 @@ func file_unified_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_unified_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   5,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
